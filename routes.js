b2b1563be66f6ca12357d23a08447cfb3d7d04fe
shanmuga priya <shanmugapriyaamsaraj@gmail.com>
2025-05-14 10:51:47 +0530

    Final update before Render deployment

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
